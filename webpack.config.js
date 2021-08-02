4fbcc7e878fc8d101046fed63a8f39b7732357f9
Constantine <golovnev1306@mail.ru>
2021-08-02 18:43:32 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+}
